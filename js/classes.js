576ff417722951c1963520da11f5328b2dad1b03
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-02-16 09:20:35 -0300

    Classes

--- js/classes.js
+++ js/classes.js
@@ -4,9 +4,13 @@ class Carro {
         this.modelo = valor2;
         this.ano = valor3;
     }
+    buzina() {
+        return this.modelo + " buzinou: Biiiiii";
+    }
 }
 
 const uno = new Carro("Fiat", "Uno", 2001);
 const celta = new Carro("GM", "Celta", 2007);
+uno.ano= 2014;
 console.log(uno);
-console.log(celta);
+console.log(celta.buzina());
