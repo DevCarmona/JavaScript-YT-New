f4b778c89e7f4dc80828d0a2b0703beaf7a52bad
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-02-17 15:52:33 -0300

    Lista de Tarefas

---
+++ js/tarefas.js
@@ -0,0 +1,68 @@
+// Icons - https://pictogrammers.com/library/mdi/
+
+let contador = 0;
+let input = document.getElementById('inputTarefa');
+let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('areaLista');
+
+function addTarefa() {
+    // Pegar o valor digitado no input
+    let valorInput = input.value;
+
+    // Se n for vazio, nem nulo, nem indefinido
+    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
+        
+        ++contador;
+        
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick= "marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick= "marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick= "deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>`;
+        // Adicionar novo item no main
+        main.innerHTML += novoItem;
+        // Zerar o campo do input
+        input.value = "";
+        input.focus(); // Fica focado no campo
+    }
+}
+
+function deletar(id) {
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    console.log(classe)
+
+    if(classe == "item") {
+        item.classList.add('clicado'); 
+
+        var icone = document.getElementById('icone_' + id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+        // Manda lá pra baixo
+        item.parentNode.appendChild(item);
+    }else {
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_' + id);
+        icone.classList.remove('mdi-check-outline');
+        icone.classList.add('mdi-circle-outline');
+    }
+}
+
+input.addEventListener("keyup", function(event) {
+    // Se teclou enter (13)
+    if(event.keyCode === 13) {
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
