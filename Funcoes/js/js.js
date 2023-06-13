c4fb1dee5613fe17a81f0996bcd8fb0ffc36d06a
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-06-13 10:42:16 -0300

    Funções - CFB Cursos

---
+++ Funcoes/js/js.js
@@ -0,0 +1,6 @@
+//  Bloco de comando que posso executar quando precisar dentro do programa.
+
+function nome () {  // Declaração da Função sem parametro ()
+    console.log("YT - CFB Cursos");
+}
+nome(); //  Chamada da função.
